83cd454df4fc6b4392c92b6128dc5a59ea4dd179
Apprentice <apprentice@dbc10.local>
2013-10-15 16:52:22 -0700

    added application js file

---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+
+  $( document ).ready(function() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+    } else {
+      console.log("no canvas context found");
+      alert("no canvas context found");
+    }
+
+    $( "#canvas" )
+
+      .mouseup(function(event) {
+      console.log("scribe stop");
+      $("#canvas").unbind('mousemove');
+    })
+
+      .mousedown(function(event) {
+      console.log("scribe start");
+
+      ctx.moveTo(event.pageX,event.pageY);
+      //on mouse move
+            $("#canvas").mousemove(function(event) {
+              ctx.lineTo(event.pageX,event.pageY);
+              ctx.stroke();
+              ctx.moveTo(event.pageX,event.pageY);
+              console.log("moving...");
+            });
+
+    });
+
+
+  });
+
+  var save = function(){
+    console.log("saving...");
+    var dataURL = canvas.toDataURL();
+    return dataURL;
+  };
+
+  var load = function(data){
+    console.log("loading...");
+    var IMG = document.createElement('img');
+    IMG.src = data;
+    IMG.width = canvas.width;
+    IMG.height = canvas.height;
+    document.body.appendChild(IMG);
+
+  };
+
+  var grab =  function() {
+    load(save());
+  }
