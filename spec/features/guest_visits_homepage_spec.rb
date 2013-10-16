19a832377cd8c0a49c1e762af5ed0dec660df9fc
Apprentice <apprentice@dbc09.local>
2013-10-16 16:36:29 -0700

    added capybara tests for homepage and login

--- spec/features/guest_visits_homepage_spec.rb
+++ spec/features/guest_visits_homepage_spec.rb
@@ -1,1 +1,22 @@
-#capybara
+require 'spec_helper'
+
+feature 'guest visits homepage' do
+        scenario "they see username and password fields" do
+                visit '/'
+                expect(page).to have_content "username:"
+                expect(page).to have_content "password:"                
+                expect(page).to have_content "Create a new Account"                
+        end
+end
+
+
+feature 'guest signs in' do
+  scenario 'they click on sign in after filling in username/pw' do
+      visit '/'
+      fill_in "username", with: "bob"
+      fill_in "password", with: "lovescake"
+      click_on "register"
+      expect(page).to have_content "welcome"
+  end
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@
 require 'capybara/rspec'
 require_relative '../app'
 require 'shoulda-matchers'
+require 'rack/test'
+
+Capybara.app = Sinatra::Application
+
