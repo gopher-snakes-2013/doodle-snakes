12c8cdbf6e4051f0e82f006e3680eddf877ff439
Apprentice <apprentice@dbc10.local>
2013-10-15 19:16:49 -0700

    added doodle_key route and database shoveling

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path('.'))
 
+require 'SecureRandom'
 require 'sinatra'
 require 'sinatra/activerecord'
 
@@ -20,7 +21,12 @@ get '/' do
 end
 
 post '/doodle_upload' do
-  Doodle.create!({doodle_data: params[:doodle_data], user_id: 5})
-  redirect '/'
+  doodle_url = SecureRandom.urlsafe_base64(12)
+  @doodle = Doodle.create!({doodle_data: params[:doodle_data], user_id: 27, doodle_key: doodle_url})
+  redirect to("/doodle/#{doodle_url}")
 end
 
+get '/doodle/:doodle_key' do
+  @doodle = Doodle.find_by_doodle_key(params[:doodle_key])
+  erb :doodle_detail
+end
