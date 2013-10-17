d7676ac7ee514b76f3b5f4535656ed0bde4febfe
Apprentice <apprentice@dbc09.local>
2013-10-16 19:48:23 -0700

    added color dropdown menu and ability to change colors mid drawing

--- app.rb
+++ app.rb
@@ -21,33 +21,45 @@ get '/' do
   erb :index
 end
 
-post '/doodle_upload' do
-  doodle_url = SecureRandom.urlsafe_base64(12)
-  @doodle = Doodle.create!({doodle_data: params[:doodle_data], user_id: 27, doodle_key: doodle_url})
-  redirect to("/doodle/#{doodle_url}")
-end
 
-get '/doodle/:doodle_key' do
-  @doodle = Doodle.find_by_doodle_key(params[:doodle_key])
-  erb :doodle_detail
-end
 
-get '/welcome' do
-  erb :welcome
-end
 
-get '/register' do
-  erb :form_register
-end
 
-post '/login' do
 
-  redirect '/welcome'
-end
 
-post '/register_user' do
-  @user = User.create!({username:params[:username] , email:params[:email] , pass:params[:pass]});
-  redirect '/welcome'
-end
+
+
+
+
+
+
+# post '/doodle_upload' do
+#   doodle_url = SecureRandom.urlsafe_base64(12)
+#   @doodle = Doodle.create!({doodle_data: params[:doodle_data], user_id: 27, doodle_key: doodle_url})
+#   redirect to("/doodle/#{doodle_url}")
+# end
+
+# get '/doodle/:doodle_key' do
+#   @doodle = Doodle.find_by_doodle_key(params[:doodle_key])
+#   erb :doodle_detail
+# end
+
+# get '/welcome' do
+#   erb :welcome
+# end
+
+# get '/register' do
+#   erb :form_register
+# end
+
+# post '/login' do
+
+#   redirect '/welcome'
+# end
+
+# post '/register_user' do
+#   @user = User.create!({username:params[:username] , email:params[:email] , pass:params[:pass]});
+#   redirect '/welcome'
+# end
 
 
---
+++ public/js/app_raw.js
@@ -0,0 +1,79 @@
+function colorSelector(){
+  color = document.getElementById("color_id");
+  console.log(color.value);
+}
+
+
+//page will not be manipulated unless document is "ready". also called a "ready event". encapsulates all the jquery.
+$( document ).ready(function() {
+  var canvas = document.getElementById('canvas');
+//context refers to a canvas' drawing context, AKA where all the drawing methods and properties will be defined.
+if (canvas.getContext){
+  var ctx = canvas.getContext('2d');
+//if the canvas does have a context, set it to 2d. apparently 3d is a possible future feature or some shit.
+} else {
+  console.log("no canvas context found");
+  alert("no canvas context found");
+}
+
+
+//on the mousedown event, begin the startMoving function.
+canvas.addEventListener("mousedown",startMoving,false);
+
+function startMoving(){
+  ctx.beginPath();
+
+
+  //sets our initial position on the canvas && registers where the drawing is going to begin.
+  var x = event.x; 
+  var y = event.y; 
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+  //moves the "pen" to the paper
+  ctx.moveTo(x,y);
+
+  //waits until the mousemove event occurs--then run draw
+  canvas.addEventListener("mousemove",draw,false);
+
+
+}
+
+
+function draw(event){
+  
+  var x = event.x; 
+  var y = event.y; 
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  //from our initialized position on the canvas, 
+  //draw a line to the x,y coords of a mousemove event. then move the "pen" to that spot. 
+  //stroke is just so that we dont make an invisible line
+  
+  ctx.lineTo(x,y);
+  ctx.moveTo(x,y);
+  ctx.stroke();
+  ctx.strokeStyle = color.value;
+  
+
+}
+
+//wait for the drag to stop, run stopMoving
+canvas.addEventListener("mouseup",stopMoving,false);
+
+//removes the mousemove listener on mouseup. now you can move the mouse freely without the draw function being called
+function stopMoving(){
+  canvas.removeEventListener("mousemove",draw,false);
+  ctx.closePath();
+}
+
+
+});
+
+
+
+
+
+
+
+
