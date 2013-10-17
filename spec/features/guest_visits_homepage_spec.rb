fde025492eaae4c0ce13cff92641299644a12cdd
Apprentice <apprentice@dbc09.local>
2013-10-16 17:10:24 -0700

    added additional capybara tests

--- spec/features/guest_visits_homepage_spec.rb
+++ spec/features/guest_visits_homepage_spec.rb
@@ -11,12 +11,33 @@ end
 
 
 feature 'guest signs in' do
-  scenario 'they click on sign in after filling in username/pw' do
+  background do
+    User.create!(:email => 'bob@gmail.com', :username =>'bob', :password_hash => 'lovescake')
+  end
+
+  scenario 'they see a page with account information' do
       visit '/'
       fill_in "username", with: "bob"
       fill_in "password", with: "lovescake"
       click_on "register"
-      expect(page).to have_content "welcome"
+      expect(page).to have_content "welcome #{:username}"
   end
+end
+
+
+feature 'guest creates account' do
+  scenario 'they fill in info' do
+    visit '/register'
+      fill_in "username", with: "bob"
+      fill_in "password", with: "lovescake"
+      fill_in "email", with: "bob@gmail.com"
+
+      click_on "register"
+      expect(page).to have_content "Welcome 'bob'"
+  end
+
+  scenario 'they click on one of the canvases theyve made before' do 
+  end
+
 end
 
